bbfa1618a615714c704295937982684f4c82e6f8
dev <developer.stoyanov@gmail.com>
2024-01-31 21:00:34 +0200

    Refactoring

--- app/src/main/java/io/architecture/playground/MainActivity.kt
+++ app/src/main/java/io/architecture/playground/MainActivity.kt
@@ -1,23 +1,16 @@
 package io.architecture.playground
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
-import io.architecture.playground.data.remote.websocket.WebSocketDiverService
 import io.architecture.playground.feature.map.MapScreen
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    // TODO
-    @Inject lateinit var webSocketService: WebSocketDiverService
-
-    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
