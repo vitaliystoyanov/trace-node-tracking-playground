2d8c4bf04961deefa13bc49b06ce19b35e91906a
dev <developer.stoyanov@gmail.com>
2024-02-05 20:42:35 +0200

    Fix coroutine dispatcher in use case

--- app/src/main/java/io/architecture/playground/domain/ObserveAndStoreRoutesUseCase.kt
+++ app/src/main/java/io/architecture/playground/domain/ObserveAndStoreRoutesUseCase.kt
@@ -3,6 +3,7 @@ package io.architecture.playground.domain
 import android.util.Log
 import io.architecture.playground.data.repository.interfaces.RouteRepository
 import io.architecture.playground.di.DefaultDispatcher
+import io.architecture.playground.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
@@ -11,10 +12,10 @@ import javax.inject.Inject
 
 class ObserveAndStoreRoutesUseCase @Inject constructor(
     private var routesRepository: RouteRepository,
-    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
 
-    suspend operator fun invoke() = withContext(defaultDispatcher) {
+    suspend operator fun invoke() = withContext(ioDispatcher) {
         routesRepository.observeAndStoreRoutes()
             .catch { error -> Log.d("SERVICE", "Error - $error") }
             .collect()
