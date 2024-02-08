23ae780e987ad74026dbe6db0313e4bfe1043950
dev <developer.stoyanov@gmail.com>
2024-02-08 20:44:17 +0200

    Array of primitives instead of boxed Double

--- app/src/main/java/io/architecture/playground/data/remote/model/NetworkRoute.kt
+++ app/src/main/java/io/architecture/playground/data/remote/model/NetworkRoute.kt
@@ -5,5 +5,26 @@ import com.google.gson.annotations.SerializedName
 
 data class NetworkRoute(
     @SerializedName("n") var nodeId: String,
-    @SerializedName("r") var route: ArrayList<ArrayList<Double>>? // TODO Array of array is better. Avoiding autoboxing
-)
+    @SerializedName("r") var route: Array<DoubleArray>?
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NetworkRoute
+
+        if (nodeId != other.nodeId) return false
+        if (route != null) {
+            if (other.route == null) return false
+            if (!route.contentDeepEquals(other.route)) return false
+        } else if (other.route != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = nodeId.hashCode()
+        result = 31 * result + (route?.contentDeepHashCode() ?: 0)
+        return result
+    }
+}
